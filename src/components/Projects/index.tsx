a8dadc8525c41c296d1177f1403205cdecd7530b
Anton Kashuba <anton.kashuba@gmail.com>
2026-02-22 12:24:41 +0200

    Change projects data, remove unnecessary img

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -8,40 +8,40 @@ import { projectsData } from "../../constants";
 import "./Projects.scss";
 
 const Projects: React.FC = () => {
-  const [visibleProjects, setVisibleProjects] = useState(6);
+	const [visibleProjects, setVisibleProjects] = useState(3);
 
-  return (
-    <section className="projects" id="projects" data-aos="fade-up">
-      <div className="container">
-        <div className="projects__header">
-          <h2 className="projects__title">Projects</h2>
-          <CtaButton text="Contact me" link="#contact" />
-        </div>
+	return (
+		<section className="projects" id="projects" data-aos="fade-up">
+			<div className="container">
+				<div className="projects__header">
+					<h2 className="projects__title">Projects</h2>
+					<CtaButton text="Contact me" link="#contact" />
+				</div>
 
-        <div className="projects__grid">
-          {projectsData?.slice(0, visibleProjects).map((project) => (
-            <ProjectItem
-              key={project.id}
-              img={project.img}
-              name={project.name}
-              techList={project.techList}
-              liveLink={project.liveLink}
-              gitRepo={project.gitRepo}
-            />
-          ))}
-        </div>
+				<div className="projects__grid">
+					{projectsData?.slice(0, visibleProjects).map((project) => (
+						<ProjectItem
+							key={project.id}
+							img={project.img}
+							name={project.name}
+							techList={project.techList}
+							liveLink={project.liveLink}
+							gitRepo={project.gitRepo}
+						/>
+					))}
+				</div>
 
-        {projectsData.length > visibleProjects && (
-          <button
-            className="btn load-more"
-            onClick={() => setVisibleProjects((prevValue) => prevValue + 3)}
-          >
-            Load more
-          </button>
-        )}
-      </div>
-    </section>
-  );
+				{projectsData.length > visibleProjects && (
+					<button
+						className="btn load-more"
+						onClick={() => setVisibleProjects((prevValue) => prevValue + 3)}
+					>
+						Load more
+					</button>
+				)}
+			</div>
+		</section>
+	);
 };
 
 export default Projects;
